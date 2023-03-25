aa8dff9b330d3a1adb00b0e3e9d904329d9d8fec
matias1483 <93952038+matias1483@users.noreply.github.com>
2023-03-24 23:48:54 -0300

    formulario php
    se creo el formulario de registro

---
+++ crud_php/formulario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+    <?php
+    $con = mysqli_connect("localhost", "root","","crud") or die ("error");
+
+    ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD PHP Y MYSQL</title>
+    </head>
+<body>
+    <form method="POST" action="formulario.php">
+        <label>Nombre</label>
+        <input type="text" name="nombre" placeholder="Escriba su nombre"><br />
+        <label>Contraseña</label>
+        <input type="password" name="passw" placeholder="Escriba su contraseña"> <br />
+        <label>Email</label>
+        <input type="email" name="email" placeholder="Escriba su mail"> <br />
+        <input type="submit" name="insert" value="insertar datos">
+    </form>
+    <?php
+        if(isset($_POST['insert'])){
+            $usuario = $_POST['nombre'];
+            $pass = $_POST['passw'];
+            $email = $_POST['email'];
+            $insertar = "INSERT INTO users (usuario, password, email) VALUES('$usuario', '$pass', '$email' )";
+
+            $ejecutar = mysqli_query($con, $insertar);
+
+            if($ejecutar){
+                echo("<h3>Insertado correctamente</h3>");
+            }
+        }
+    ?>
+
+</body>
+</html>
